76cb30375d2e13df468e4f38b4c5e7c65b28c338
xanka359 <159315307+xanka359@users.noreply.github.com>
2024-02-23 23:12:37 +0300

    Delete script_archive.py

--- script_archive.py
+++
@@ -1,7 +0,0 @@
-from zipfile import ZipFile
-
-with ZipFile("tmp/hello.zip") as zip_file:
-    print(zip_file.namelist())
-    text = zip_file.read('Hello.txt')
-    print(text)
-    zip_file.extract('Hello.txt', path="tmp")
